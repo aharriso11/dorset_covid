825c5a28585bda48ce1544805e76d98fc569c553
aharriso11 <78227435+aharriso11@users.noreply.github.com>
2021-12-20 20:14:43 +0000

    push_2021_12_20_2013

--- scripts/vaccs deprivation.R
+++ scripts/vaccs deprivation.R
@@ -30,7 +30,7 @@ df_lookup <- read.csv(url("http://geoportal1-ons.opendata.arcgis.com/datasets/fe
 vaccs_percentage_dor <- read.csv(url("https://api.coronavirus.data.gov.uk/v2/data?areaType=msoa&areaCode=E06000059&metric=cumVaccinationFirstDoseUptakeByVaccinationDatePercentage&metric=cumVaccinationSecondDoseUptakeByVaccinationDatePercentage&metric=cumVaccinationThirdInjectionUptakeByVaccinationDatePercentage&format=csv"))
 vaccs_percentage_bcp <- read.csv(url("https://api.coronavirus.data.gov.uk/v2/data?areaType=msoa&areaCode=E06000058&metric=cumVaccinationFirstDoseUptakeByVaccinationDatePercentage&metric=cumVaccinationSecondDoseUptakeByVaccinationDatePercentage&metric=cumVaccinationThirdInjectionUptakeByVaccinationDatePercentage&format=csv"))
 
-# MUNGE DATA ----
+# MUNGE VACCINATION DATA ----
 
 # merge vaccination datasets into one
 # 1 - place datasets into a single list
@@ -55,6 +55,8 @@ vaccs_percentage_long <- gather(vaccs_percentage_combined, event, total, First:l
 vaccs_percentage_long <- vaccs_percentage_long %>%
   mutate(unvacc_total = 100 - total)
 
+# MUNGE INDEX OF DEPRIVATIOM DATA ----
+
 # create a working dataframe with the Index of Multiple Deprivation columns we need
 df_work <- subset(df_iod, select = c("LSOA.code..2011.", "LSOA.name..2011.", "Local.Authority.District.code..2019.", "Local.Authority.District.name..2019.", "Index.of.Multiple.Deprivation..IMD..Score", "Total.population..mid.2015..excluding.prisoners."))
 
@@ -74,6 +76,8 @@ df_work$msoaname <- with(vaccs_percentage_long, areaName[match(df_work$msoacode,
 # following the IMD technical guidance, create a IMD multiplier by multiplying LSOA population by IMD score
 df_work <- df_work %>%
   mutate(msoa_dep_multiplier = Total.population..mid.2015..excluding.prisoners. * Index.of.Multiple.Deprivation..IMD..Score)
+
+# AGGREGATE LSOA TO MSOA
 
 # group the LSOAs by MSOA and add their population figures together - creates a new lookup table
 df_msoapops <- df_work %>%
@@ -102,20 +106,31 @@ df_msoascores$msoaimdagg <- with(df_msoaimdscore, msoaimdagg[match(df_msoascores
 df_msoascores <- df_msoascores %>%
   mutate(msoa_imd_score = msoaimdagg / msoapop)
 
+# MERGE VACCS AND DEPRIVATION DATA
+
 # add the MSOA IMD score to the long vaccination data
 vaccs_percentage_long$msoa_imd_score <- with(df_msoascores, msoa_imd_score[match(vaccs_percentage_long$areaCode, msoacode)])
 
 # add the legacy LA to the long vaccination data
 vaccs_percentage_long$legacylad <- with(df_msoascores, legacylad[match(vaccs_percentage_long$areaCode, msoacode)])
 
+# PLOT DATA ----
+
+# build plot
 unvaxxed_plot <- ggplot(data = subset(vaccs_percentage_long, event == "First")) +
+  # create scatter plot
   geom_point(aes(x = msoa_imd_score, y = unvacc_total, col = legacylad), shape = 15, size = 2) +
+  # repelling text labels
   geom_text_repel(aes(x = msoa_imd_score, y = unvacc_total, label = areaName), size = 1.75, segment.color = "red", max.overlaps = Inf) +
+  # configure legend and scatter plot colours
   scale_colour_manual(name = "Legacy local authority", values = c("Bournemouth" = "royalblue1", "Christchurch" = "grey", "East Dorset" = "orange", "North Dorset" = "darkgreen", "Poole" = "aquamarine3", "Purbeck" = "navy", "West Dorset" = "darkviolet", "Weymouth and Portland" = "green2")) +
+  # axis labels
   xlab("Deprivation score") +
   ylab("Unvaccinated percentage") +
+  # title and subtitle
   ggtitle("Comparing covid vaccination status with deprivation") +
-  labs(caption = paste("Data from UK Health Security Agency / https://coronavirus.data.gov.uk. Plotted", Sys.time(), sep = " "), subtitle = "Percentage of people aged 12 and over in a MSOA that have not yet received a first covid vaccination, plotted against the MSOA's deprivation score.") +
+  labs(caption = paste("Data from UK Health Security Agency and DLUHC / https://coronavirus.data.gov.uk. Plotted", Sys.time(), sep = " "), subtitle = "Percentage of people aged 12 and over in a MSOA that have not yet received a first covid vaccination, plotted against the MSOA's deprivation score.") +
+  # theme and theme settings
   theme_base() +
   theme(
     plot.title = element_text(size = 20, family = "Helvetica", face = "bold"),
